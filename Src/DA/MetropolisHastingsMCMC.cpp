9a1279748cac8966702ff4806903152e0166ea8b
Charles Tong <32374542+tong10@users.noreply.github.com>
2023-05-23 13:46:21 -0700

    Update MetropolisHastingsMCMC.cpp
    add acknowledgment

--- Src/DA/MetropolisHastingsMCMC.cpp
+++ Src/DA/MetropolisHastingsMCMC.cpp
@@ -20,7 +20,7 @@
 // along with this program; if not, write to the Free Software Foundation,
 // Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 // ************************************************************************
-// KPCA functions 
+// KPCA functions (supported by Xiao Chen's LDRD)
 // DATE   : 2015
 // ************************************************************************
 #include <fstream>
